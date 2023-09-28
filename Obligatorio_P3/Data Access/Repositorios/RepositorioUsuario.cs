160327d77cbaca5936d7d33e120d62e3124f5fa8
Bruno Capelli <brunoicapelli@gmail.com>
2023-09-28 19:49:50 -0300

    Add I/RepoUsuario, add I/ServicioUsuario & add EntityFramework to the project

---
+++ Obligatorio_P3/Data Access/Repositorios/RepositorioUsuario.cs
@@ -0,0 +1,42 @@
+﻿using Data_Access.IRepositorios;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access.Repositorios
+{
+    public class RepositorioUsuario : IRepositorioUsuario
+    {
+        private DbContext Context { get; set; }
+
+        public RepositorioUsuario(DbContext _context)
+        {
+            Context = _context;
+        }
+
+        public Usuario Add(Usuario entity)
+        {
+            Context.Set<Usuario>().Add(entity);
+            return entity;
+        }
+
+        public void Remove(Usuario entity)
+        {
+            Context.Set<Usuario>().Remove(entity);
+        }
+
+        public void Save()
+        {
+           Context.SaveChanges();
+        }
+
+        public void Update(Usuario entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
